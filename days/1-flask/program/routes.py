cf87ddb1716a44d955ac860e589258b88dd8306c
Jim Pfleger <jpfleger@acm.org>
2022-01-20 09:16:31 -0700

    ♻️ Make base HTML template, separate content

--- days/1-flask/program/routes.py
+++ days/1-flask/program/routes.py
@@ -1,7 +1,13 @@
+from flask import render_template
 from program import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello Pythonistas"
+    return render_template("index.html")
+
+
+@app.route("/100days")
+def hundred_days():
+    return render_template("100days.html")
